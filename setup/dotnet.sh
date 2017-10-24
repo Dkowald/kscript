739e68d2cef957a8fec7da7f40ad55400c4989f4
dkowald <derek@kowald.id.au>
2017-10-24 13:30:18 +1000

    adding dotnet install

--- setup/dotnet.sh
+++ setup/dotnet.sh
@@ -1,3 +1,9 @@
-
 #!/bin/bash
-if [ -x dotnet ]; then echo "dotnet already installed"; else echo "todo install dotnet"; fi
+if [ -x dotnet ]; 
+  then 
+    echo "dotnet already installed"; 
+  else 
+  #https://www.microsoft.com/net/core#linuxubuntu
+    echo "install dotnet";
+    ~/kscript/setup/dotnet/dotnet.add-apt.sh
+fi
---
+++ setup/dotnet/dotnet.add-apt.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Add dot net deb feed.
+# https://www.microsoft.com/net/core#linuxubuntu
+
+if [-f /etc/apt/trusted.gpg.d/microsoft.gpg];then;
+  else
+    echo "Need ms apt key"
+    mkdir -p ~/temp/kscript
+    wget https://packages.microsoft.com/keys/microsoft.asc -O ~/tmp/kscript/microsoft.gpg
+    sudo mv ~/kscript/setup/dotnet/microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+fi
+
+if [if /etc/apt/sources.list.d/dotnetdev.list]; then;
+  else
+    echo "Adding dotnet apt feed"
+    ln -s ~/kscript/setup/dotnet/dotnetdev.list /etc/apt/sources.list.d/dotnetdev.list
+fi
+
+sudo apt-get update
+
+sudo apt-get install dotnet-sdk-2.0.0
