812b15e4ab033a91f26d556ba12bf52a446e4add
derek@ubu16 <derek@ubu16.local.net>
2017-10-26 16:36:51 +1000

    got a dotnet 2 install in

--- setup/dotnet.d/addKey.sh
+++ setup/dotnet.d/addKey.sh
@@ -3,7 +3,7 @@ if [ -f /etc/apt/trusted.gpg.d/microsoft.gpg ]
     echo "key already installed"
   else
     echo "installing key"
-    curl https://packages.microsoft.com/keys/microsoft.asc > ~/tmp/microsoft.gpg
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > ~/tmp/microsoft.gpg
     sudo mv ~/tmp/microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 fi
 
