68b99b3c503acb8a516e1fb4459939c6e82cb59c
dkowald <derek@kowald.id.au>
2017-11-28 16:27:06 +1000

    start looking at scripts for ravendb

---
+++ setup/ravendb/install.sh
@@ -0,0 +1,15 @@
+#add service config (chage to link?)
+cp ravendb.service /etc/systemd/system/ravendb.service
+
+#refresh deamon info
+sudo systemctl daemon-reload
+sudo systemctl restart
+
+#enable  the service
+sudo systemctl enable ravendb
+
+#start the service
+sudo systemctl start ravendb
+
+#check status
+systemctl status ravendb.service
